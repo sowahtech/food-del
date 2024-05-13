a5620e1675424ed2842eabd7f4bfe077d5d7efe1
sowahtech <sowahtech@gmail.com>
2024-05-13 17:32:57 +0000

    started food delivery app and created navbar component

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,79 @@
+import logo from './logo.png'
+
+import menu_1 from './burger1.avif'
+import menu_2 from './pasta1.avif'
+import menu_3 from './pitza4.jpg'
+import menu_4 from './supper2.avif'
+
+import food_1 from './supper2.avif'
+import food_2 from './pasta2.avif'
+import food_3 from './pitza5.jpg'
+import food_4 from './pasta1.avif'
+import food_5 from './supper2.avif'
+import food_6 from './pasta2.avif'
+import food_7 from './pitza5.jpg'
+import food_8 from './pasta1.avif'
+import food_9 from './supper2.avif'
+import food_10 from './pasta2.avif'
+import food_11 from './pitza5.jpg'
+import food_12 from './pasta1.avif'
+import food_13 from './supper2.avif'
+import food_14 from './pasta2.avif'
+import food_15 from './pitza5.jpg'
+import food_16 from './pasta1.avif'
+import food_17 from './supper2.avif'
+import food_18 from './pasta2.avif'
+import food_19 from './pitza5.jpg'
+import food_20 from './noodles.avif'
+import food_21 from './supper2.avif'
+import food_22 from './pasta2.avif'
+import food_23 from './pitza5.jpg'
+import food_24 from './noodles.avif'
+import food_25 from './supper2.avif'
+import food_26 from './pasta2.avif'
+import food_27 from './pitza5.jpg'
+import food_28 from './noodles.avif'
+import food_29 from './supper2.avif'
+import food_30 from './pasta2.avif'
+import food_31 from './pitza5.jpg'
+import food_32 from './noodles.avif'
+
+export const assets = {
+	logo,
+	menu1,
+	menu2,
+	menu3,
+	menu4,
+	food1,
+	food2,
+	food3,
+	food4,
+	food5,
+	food6,
+	food7,
+	food8,
+	food9,
+	food10,
+	food11,
+	food12, 
+	food13,
+	food14, 
+	food15,
+	food16,
+	food17,
+	food18,
+	food19,
+	food20,
+	food21,
+	food22,
+	food23,
+	food24,
+	food25,
+	food26,
+	food27,
+	food28,
+	food29,
+	food30,
+	food31,
+	food32
+};
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import { FaSearch } from "react-icons/fa";
+import { IoIosBasket } from "react-icons/io";
+
+const Navbar = () => {
+  const [menu, setMenu] = useState("home");
+
+  return (
+    <div className="navbar">
+      <img src={logo} alt="" className="logo" />
+      <ul className="navbar_menu">
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          home
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </li>
+        <li
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          mobile-app
+        </li>
+        <li
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          contact us
+        </li>
+      </ul>
+      <div className="navbar_right">
+        <FaSearch />
+        <div className="navbar_search_icon">
+          <IoIosBasket />
+          <div className="dot"></div>
+        </div>
+        <button>sign in</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
