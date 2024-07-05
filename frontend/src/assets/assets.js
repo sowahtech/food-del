22ae9efb78f479f339a721401d9973307ae2349d
sowahtech <sowahtech@gmail.com>
2024-07-05 18:07:25 +0000

    explore menu component added to food delivery app

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,79 +1,49 @@
-import logo from './logo.png'
+import logo from "./logo.png";
 
-import menu_1 from './burger1.avif'
-import menu_2 from './pasta1.avif'
-import menu_3 from './pitza4.jpg'
-import menu_4 from './supper2.avif'
-
-import food_1 from './supper2.avif'
-import food_2 from './pasta2.avif'
-import food_3 from './pitza5.jpg'
-import food_4 from './pasta1.avif'
-import food_5 from './supper2.avif'
-import food_6 from './pasta2.avif'
-import food_7 from './pitza5.jpg'
-import food_8 from './pasta1.avif'
-import food_9 from './supper2.avif'
-import food_10 from './pasta2.avif'
-import food_11 from './pitza5.jpg'
-import food_12 from './pasta1.avif'
-import food_13 from './supper2.avif'
-import food_14 from './pasta2.avif'
-import food_15 from './pitza5.jpg'
-import food_16 from './pasta1.avif'
-import food_17 from './supper2.avif'
-import food_18 from './pasta2.avif'
-import food_19 from './pitza5.jpg'
-import food_20 from './noodles.avif'
-import food_21 from './supper2.avif'
-import food_22 from './pasta2.avif'
-import food_23 from './pitza5.jpg'
-import food_24 from './noodles.avif'
-import food_25 from './supper2.avif'
-import food_26 from './pasta2.avif'
-import food_27 from './pitza5.jpg'
-import food_28 from './noodles.avif'
-import food_29 from './supper2.avif'
-import food_30 from './pasta2.avif'
-import food_31 from './pitza5.jpg'
-import food_32 from './noodles.avif'
+import menu_1 from "./burger1.jpeg";
+import menu_2 from "./fried-rice1.jpeg";
+import menu_3 from "./jollof2.jpeg";
+import menu_4 from "./noodles1.jpeg";
+import menu_5 from "./pitza1.jpeg";
+import menu_6 from "./salad1.jpeg";
+import menu_7 from "./rolls1.jpeg";
+import menu_8 from "./cake1.jpeg";
 
 export const assets = {
-	logo,
-	menu1,
-	menu2,
-	menu3,
-	menu4,
-	food1,
-	food2,
-	food3,
-	food4,
-	food5,
-	food6,
-	food7,
-	food8,
-	food9,
-	food10,
-	food11,
-	food12, 
-	food13,
-	food14, 
-	food15,
-	food16,
-	food17,
-	food18,
-	food19,
-	food20,
-	food21,
-	food22,
-	food23,
-	food24,
-	food25,
-	food26,
-	food27,
-	food28,
-	food29,
-	food30,
-	food31,
-	food32
+  logo,
 };
+
+export const menu_list = [
+  {
+    menu_name: "burger",
+    menu_image: menu_1,
+  },
+  {
+    menu_name: "fried rice",
+    menu_image: menu_2,
+  },
+  {
+    menu_name: "jollof",
+    menu_image: menu_3,
+  },
+  {
+    menu_name: "noodles",
+    menu_image: menu_4,
+  },
+  {
+    menu_name: "pitza",
+    menu_image: menu_5,
+  },
+  {
+    menu_name: "Salad",
+    menu_image: menu_6,
+  },
+  {
+    menu_name: "Rolls",
+    menu_image: menu_7,
+  },
+  {
+    menu_name: "Cake",
+    menu_image: menu_8,
+  },
+];
