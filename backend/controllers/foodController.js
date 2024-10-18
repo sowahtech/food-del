ec23ecfbd2a100187e12cb931741073b2bb89b89
sowahtech <sowahtech@gmail.com>
2024-10-18 11:20:37 +0000

    remove food endpoint working successfully

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -34,4 +34,19 @@ const listFood = async (req, res) => {
   }
 };
 
-export { addFood, listFood };
+// remove food item
+
+const removeFood = async (req, res) => {
+  try {
+    const food = await foodModel.findById(req.body.id);
+    fs.unlink(`uploads/${food.image}`, () => {});
+
+    await foodModel.findByIdAndDelete(req.body.id);
+    res.json({ success: true, message: "Food removed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "error" });
+  }
+};
+
+export { addFood, listFood, removeFood };
