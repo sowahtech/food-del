e7b0a1ef4dfc55664a87b0a1e0fdcdf2ddedbc4d
sowahtech <sowahtech@gmail.com>
2024-12-06 23:33:13 +0000

    successfully integrated cart api with frontend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -27,7 +27,7 @@ const loginUser = async (req, res) => {
 };
 
 const createToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+  return jwt.sign({ id }, process.env.JWT_SECRET);
   console.log(process.env.JWT_SECRET);
 };
 
