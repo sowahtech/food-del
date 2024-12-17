e32ebda5dee0097ec99240b311e4992c64bb4726
sowahtech <sowahtech@gmail.com>
2024-12-17 20:29:30 +0000

    integrated list orders API with admin orders component

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import "./Orders.css";
-const Orders = () => {
-  return <div>Orders</div>;
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const Orders = ({ url }) => {
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllOrders = async () => {
+    const response = await axios.get(url + "/api/order/list");
+    if (response.data.success) {
+      setOrders(response.data.data);
+      console.log(response.data.data);
+    } else {
+      toast.error("Error");
+    }
+  };
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, []);
 };
 
 export default Orders;
