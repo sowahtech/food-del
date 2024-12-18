b67a68d829c3a9cd25afec6a5b90708c3ff5a1c5
sowahtech <sowahtech@gmail.com>
2024-12-18 18:20:10 +0000

    orders page in admin panel created

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -2,7 +2,8 @@ import { useState, useEffect } from "react";
 import "./Orders.css";
 import axios from "axios";
 import { toast } from "react-toastify";
-
+import PropTypes from "prop-types";
+import { assets } from "./../../assets/assets";
 const Orders = ({ url }) => {
   const [orders, setOrders] = useState([]);
 
@@ -19,6 +20,56 @@ const Orders = ({ url }) => {
   useEffect(() => {
     fetchAllOrders();
   }, []);
+
+  return (
+    <div className="order add">
+      <h3>Order Page</h3>
+      <div className="order-list">
+        {orders.map((order, index) => (
+          <div key={index} className="order-item">
+            <img src={assets.parcel_icon} alt="" />
+            <div>
+              <p className="order-item-food">
+                {order.items.map((item, index) => {
+                  if (index === order.items.length - 1) {
+                    return item.name + " x " + item.quantity;
+                  } else {
+                    return item.name + " x " + item.quantity + ", ";
+                  }
+                })}
+              </p>
+              <p className="order-item-name">
+                {order.address.firstName + " " + order.address.lastName}
+              </p>
+              <div className="order-item-address">
+                <p>{order.address.street + ","}</p>
+                <p>
+                  {order.address.city +
+                    ", " +
+                    order.address.state +
+                    ", " +
+                    order.address.country +
+                    ", " +
+                    order.address.zipcode}
+                </p>
+              </div>
+              <p className="order-item-phone">{order.address.phone}</p>
+            </div>
+            <p>Items: {order.items.length}</p>
+            <p>${order.amount}</p>
+            <select>
+              <option value="Food processing">Food processing</option>
+              <option value="Out for delivery">Out for delivery</option>
+              <option value="Delivered">Delivered</option>
+            </select>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
+Orders.propTypes = {
+  url: PropTypes.string.isRequired,
+};
 export default Orders;
