0385d18deffb2d1821e6e93467087a914014a56a
sowahtech <sowahtech@gmail.com>
2024-12-18 20:10:14 +0000

    changed frontend url from port 5173 to port 5174

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing order from frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+  const frontend_url = "http://localhost:5174";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
