dcbdae66068a322c5372b34f82bd7080a88d6245
sowahtech <sowahtech@gmail.com>
2026-02-13 17:23:56 +0000

    removed mongodb uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,3 @@
-// import mongoose from "mongoose";
-
-// export const connectDB = async () => {
-//   await mongoose
-//     .connect(
-//       "mongodb+srv://justicesowah18:complete092024@cluster0.0wkwf.mongodb.net/oregano-food-del-app"
-//     )
-//     .then(() => console.log("connected to DB successfully"));
-// };
-// backend/config/db.js
 
 import mongoose from "mongoose";
 
