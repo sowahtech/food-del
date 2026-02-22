f1cb7ab3fcb20eb6730ccbf9739d53f9a734695f
Justice Sowah <sowahtech@gmail.com>
2026-02-22 18:11:27 +0000

    configured admin/vite.config.js to include react

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -2,12 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// }),
-
 export default defineConfig({
-  base: '/admin/', // This ensures assets load from /admin/assets/...
-  // ...other config
+  plugins: [react()],
+  build: {
+    outDir: "dist"
+  }
 })
-
