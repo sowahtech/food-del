b12ad6f9526a56ff4b8b74e1ff6e2ed028b3091d
Justice Sowah <sowahtech@gmail.com>
2026-02-22 17:55:35 +0000

    edited frontend url

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ app.get("/admin/*", (req, res) => {
 
 // Frontend route
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
