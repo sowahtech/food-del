220beca4245eb8a7dd7aa6aa4415e4853678fbaa
Justice Sowah <sowahtech@gmail.com>
2026-02-22 14:37:30 +0000

    added base property in admin vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// }),
+
 export default defineConfig({
-  plugins: [react()],
+  base: '/admin/', // This ensures assets load from /admin/assets/...
+  // ...other config
 })
+
