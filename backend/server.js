c1d0c7a39993b74450d4337b062a540fa995238a
Justice Sowah <sowahtech@gmail.com>
2026-03-11 23:33:44 +0000

    commented out dns configurations

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-import { setServers } from 'node:dns/promises';
-setServers(['1.1.1.1', '8.8.8.8']);
+// import { setServers } from 'node:dns/promises';
+// setServers(['1.1.1.1', '8.8.8.8']);
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
