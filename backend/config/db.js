847c76125f3c9b6f64db7f1d9400379c29fdfac5
Justice Sowah <sowahtech@gmail.com>
2026-03-11 22:41:21 +0000

    edited db.js to display error message

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ export const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (err) {
-    console.error(err);
+    console.error(error.message);
     process.exit(1);
   }
 };
